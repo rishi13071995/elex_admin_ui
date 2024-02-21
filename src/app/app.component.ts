b46c2271bf781c69dff27206039e7365a6514ce0
rishiprakashmishra <rishi13071995@gmail.com>
2024-02-21 22:50:08 +0530

    Login design changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-
+import { FormsModule }   from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
--- src/app/icons-provider.ts
+++ src/app/icons-provider.ts
@@ -11,11 +11,13 @@ import {
   LogoutOutline,
   LoginOutline,
   UploadOutline,
-  ArrowLeftOutline
+  ArrowLeftOutline,
+  FundProjectionScreenOutline,
+  LockOutline
 } from '@ant-design/icons-angular/icons';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
-const icons = [MenuFoldOutline, MenuUnfoldOutline, DashboardOutline, FormOutline, UserOutline, OrderedListOutline, FolderOpenOutline, SettingOutline, LogoutOutline, LoginOutline, ArrowLeftOutline];
+const icons = [MenuFoldOutline, MenuUnfoldOutline, DashboardOutline, FormOutline, UserOutline, OrderedListOutline, FolderOpenOutline, SettingOutline, LogoutOutline, LoginOutline, ArrowLeftOutline, LockOutline,FundProjectionScreenOutline];
 
 export function provideNzIcons(): EnvironmentProviders {
   return importProvidersFrom(NzIconModule.forRoot(icons));
